3867f3b3015b53146a77503ec68e60acf78d89d5
RickeyZh <rickey.zhengg@gmail.com>
2023-08-26 00:08:59 -0400

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,13 +8,13 @@ function Navi() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Insert Name</Navbar.Brand>
+        <Navbar.Brand href="/">Insert Name</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavLink to="/student"> <Nav.Link href="#home">Student</Nav.Link></NavLink>
+            <Nav.Link href="/student">Student</Nav.Link>
           
-            <NavLink to="/teacher"> <Nav.Link href="#link">Teacher</Nav.Link></NavLink>
+            <Nav.Link href="/teacher">Teacher</Nav.Link>
           
             <NavDropdown title="Options" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
