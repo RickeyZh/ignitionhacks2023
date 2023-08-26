9db7a7e45b5eaf70d5d682651af6a4aebe22798e
Andrew-Yeung <ayuuu9102@gmail.com>
2023-08-26 00:41:06 -0400

    align text

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,14 +2,18 @@ import { Link } from "react-router-dom";
 import Particles from "../Components/Particles";
 import Typewriter from "../Components/TypingFont";
 import "../App.css";
+
 export default function Home() {
   return (
     
     <div>
         <Particles />
+        <center>
         <h1>
-        Welcome to <Typewriter text="OUR WEBSITE" delay={100} infinite />
-      </h1>
+        Welcome to <Typewriter text="OUR WEBSITE" delay={250} infinite />
+        </h1>
+        </center>
+        
     </div>
     
   )
