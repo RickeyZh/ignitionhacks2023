7b9db35932ea85112e301782eaff9a50798ea8b2
Eric Chen <70295495+cloudec11@users.noreply.github.com>
2023-08-25 22:37:33 -0400

    navbar and deployment commands

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+
 import Teacher from "./Pages/Teacher";
 import Student from "./Pages/Student";
+
+import Navbar from "./Components/Navbar";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <div className="">
+        <Navbar />
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/teacher" element={<Teacher />} />
