f36710ee561a53b349646809c97d01d249e467c6
Andrew-Yeung <ayuuu9102@gmail.com>
2023-08-26 00:21:30 -0400

    particle in other page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import Particles from "../Components/Particles";
 
 export default function About() {
     return (
         <div>
+            <Particles />
             this is how to use the website.
         </div>
     )
