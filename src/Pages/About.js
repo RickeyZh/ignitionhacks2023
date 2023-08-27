2aecfb170404da74f425acc8616b56370003b04b
Andrew-Yeung <ayuuu9102@gmail.com>
2023-08-27 01:28:58 -0400

    about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,7 +4,18 @@ export default function About() {
     return (
         <div>
             <Particles />
-            this is how to use the website.
+            <p className></p>
+            <h1 className="smalllogo1">Edu</h1>
+            <h1 className="smalllogo2">Link</h1>
+            <p className="desctext">
+                <p></p>
+            EduLink is a user-friendly website that improves the everyday lives of teachers and students. 
+            With the use of artificial intelligence and face recognition API,
+             EduLink allows teachers to gauge how students are feeling by 
+             providing them with a summary of how the audience's emotions.
+             This solves the problem of online learning/presenting, 
+             where at times it is hard to measure an especially large audience’s attention. 
+            </p>
         </div>
     )
 }
