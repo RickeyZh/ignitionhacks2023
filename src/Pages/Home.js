6d580dd3cf5ac60149b056439dc1f3ceb5ffe71d
Eric Chen <70295495+cloudec11@users.noreply.github.com>
2023-08-27 00:38:26 -0400

    fancy buttons on home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,28 +2,24 @@ import { Link } from "react-router-dom";
 import Particles from "../Components/Particles";
 import Typewriter from "../Components/TypingFont";
 import "../App.css";
+import "../buttons.scss";
 //type
 import ReactTyped from "react-typed";
 
-
 export default function Home() {
   return (
-  
     <div>
-        <Particles />
-        <h1 className="title-font">
-      Welcome to  
-        </h1>
-        <h1 className="title-font-second">
-      Edu
-        </h1>
-        <h1 className="title-font-third">
-      Link
-        </h1>
-        <h1 className="desc">
+      <Particles />
+      <h1 className="title-font">Welcome to</h1>
+      <h1 className="title-font-second">Edu</h1>
+      <h1 className="title-font-third">Link</h1>
+      <h1 className="desc">
         {" "}
         <ReactTyped
-          strings={["Keeping Teachers Emotionally in Tune with Students", "Check our the About page for more info"]}
+          strings={[
+            "Keeping Teachers Emotionally in Tune with Students",
+            "Check our the About page for more info",
+          ]}
           typeSpeed={20}
           loop
           backSpeed={20}
@@ -31,9 +27,12 @@ export default function Home() {
           showCursor={true}
         />
       </h1>
-        
+      <a class="btn-1" href="#">
+        Students
+      </a>
+      <a class="btn-1" href="#">
+        Teachers
+      </a>
     </div>
-    
-  )
+  );
 }
-
