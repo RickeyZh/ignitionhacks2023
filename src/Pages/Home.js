1153e249a9dd47be407a4b808bd5b15e2ab5bf9b
Andrew-Yeung <ayuuu9102@gmail.com>
2023-08-26 23:52:31 -0400

    typing animaton on homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 import Particles from "../Components/Particles";
 import Typewriter from "../Components/TypingFont";
 import "../App.css";
+//type
+import ReactTyped from "react-typed";
+
 
 export default function Home() {
   return (
@@ -17,9 +20,17 @@ export default function Home() {
         <h1 className="title-font-third">
       Link
         </h1>
-        
-        
-      
+        <h1 className="desc">
+        {" "}
+        <ReactTyped
+          strings={["Keeping Teachers Emotionally in Tune with Students", "Check our the About page for more info"]}
+          typeSpeed={20}
+          loop
+          backSpeed={20}
+          cursorChar=">"
+          showCursor={true}
+        />
+      </h1>
         
     </div>
     
